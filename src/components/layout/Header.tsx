841ae82d3ef66fc21c4221d03a48d7ca92d5429f
igor.quintao <igor.quintao@edetek.com>
2025-10-16 18:16:17 -0300

    Test change

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,7 +12,7 @@ export function Header() {
           <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
           <Input
             type="search"
-            placeholder="Search..."
+            placeholder="Search.."
             className="pl-10"
           />
         </div>
